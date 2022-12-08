6205aa9259b5f1b63ed218c0afa8460c0f096111
Fadlin Azhary <fadlinsiregar@gmail.com>
2022-12-08 17:25:21 +0700

    Menyusun Package

---
+++ app/src/main/java/com/example/secretnotes/helpers/Config.kt
@@ -0,0 +1,8 @@
+package com.example.secretnotes.helpers
+
+class Config {
+    companion object {
+        const val SPLASH_SCREEN_TIME: Long = 4000
+        const val NOTES_API_URL = "api."
+    }
+}
